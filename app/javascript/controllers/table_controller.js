0186beef7f6c728c63c8d3c3961ff88acb593142
Yev Matveyev <yev_mtv@icloud.com>
2023-10-16 19:20:10 +0700

    Fix UI errors. Products in table now clickable for better UX

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -5,8 +5,16 @@ export default class extends Controller {
   goTo(event) {
     if (event.target.className === "actions") return;
     if (event.target.parentNode.className === "actions") return;
+
     event.preventDefault();
+
     const url = window.location.origin + event.params.url;
+
+    if (event.metaKey || event.ctrlKey) {
+      window.open(url, "_blank");
+      return;
+    }
+
     window.location.assign(url);
   }
 }
