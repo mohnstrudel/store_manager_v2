df461c4961594807b7c6991f04739912396d1318
Yev Matveyev <yev_mtv@icloud.com>
2023-11-20 16:33:54 +0800

    Add test cases to ensure we use all the info from the API to create new products

--- spec/jobs/sync_woo_products_job_spec.rb
+++ spec/jobs/sync_woo_products_job_spec.rb
@@ -9,9 +9,24 @@ RSpec.describe SyncWooProductsJob do
 
   describe "#save_woo_products_to_db" do
     context "when we parsed products from Woo API" do
-      it "saves each product to the DB" do
+      before do
         job.save_woo_products_to_db(parsed_woo_products)
+      end
+
+      it "saves each product to the DB" do
         expect(Product.all.size).to eq(parsed_woo_products.size)
+      end
+
+      it "creates products with all parsed data" do
+        first_created = Product.first
+        first_parsed = parsed_woo_products.first
+        expect(first_created.title).to eq(first_parsed[:title])
+        expect(first_created.woo_id).to eq(first_parsed[:woo_id].to_s)
+        expect(first_created.shape.title).to eq(first_parsed[:shape])
+        expect(first_created.versions.size).to eq(first_parsed[:versions].size)
+        expect(first_created.brands.size).to eq(first_parsed[:brands].size)
+        expect(first_created.sizes.size).to eq(first_parsed[:sizes].size)
+        expect(first_created.colors.size).to eq(first_parsed[:colors].size)
       end
     end
   end
