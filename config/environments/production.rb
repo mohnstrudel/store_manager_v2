b9e5e05c22451d5c9bf25355adb8bb39ae02a519
Yevgeny Matveyev <yev_mtv@icloud.com>
2024-02-18 11:38:07 +0530

    Try to fix code loading for rake tasks in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Eager load the application when running Rake tasks
+  config.rake_eager_load = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
