4ca0e6e6e0c70f6108c30ff986206b227f0db0d3
Yevgeny Matveyev <yev_mtv@icloud.com>
2024-02-20 13:06:39 +0530

    Add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    http_basic_authenticate_with(
+      name: Rails.application.credentials.dig(:basic_auth, :log),
+      password: Rails.application.credentials.dig(:basic_auth, :pas)
+    )
+  end
+
   unless Rails.env.production?
     around_action :n_plus_one_detection
 
