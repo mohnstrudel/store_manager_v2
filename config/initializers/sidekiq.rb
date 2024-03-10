bdcf765f7bc88a7c3e84ae87daa44b3a9fb4c5e7
Yevgeny Matveyev <yev_mtv@icloud.com>
2024-03-10 15:48:07 +0530

    Fix broken Turbo support in Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,12 @@ end
 Sidekiq.configure_client do |config|
   config.redis = {ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}}
 end
+
+# Fix for Turbo+Sidekiq from: https://github.com/hotwired/turbo-rails/issues/535
+Rails.application.config.after_initialize do
+  Turbo::Streams::BroadcastStreamJob.class_eval do
+    def self.perform_later(stream, content:)
+      super(stream, content: content.to_str)
+    end
+  end
+end
