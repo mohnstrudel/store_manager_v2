3990f18a012c23ef24784095b834888d459aa618
Yevgeny Matveyev <yev_mtv@icloud.com>
2024-04-12 19:51:42 +0800

    Fix incorrect partial requirement and local storage config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   end
 
   if Rails.env.development?
+    ActiveStorage::Current.url_options = {host: "http://localhost:3000"}
+
     around_action :n_plus_one_detection
 
     def n_plus_one_detection
