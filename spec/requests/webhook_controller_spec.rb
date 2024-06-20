4607a75d2e14ac93cea9e53004aee494997e8a47
Yevgeny Matveyev <yev_mtv@icloud.com>
2024-06-20 17:39:53 +0800

    Test that we create new orders with our webhook

--- spec/requests/webhook_controller_spec.rb
+++ spec/requests/webhook_controller_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe WebhookController do
   }
 
   # rubocop:disable RSpec/ExampleLength
-  describe "updates sales on /post" do
-    before "mock a sale" do
+  describe "when we receive a payload from Webhook" do
+    before do
       customer = create(:customer, woo_id: parsed_order[:customer][:woo_id])
       sale = create(:sale, woo_id: parsed_order[:sale][:woo_id], customer:)
       product = create(:product, woo_id: parsed_order[:products].first[:product_woo_id])
@@ -24,18 +24,36 @@ RSpec.describe WebhookController do
         price: 666,
         woo_id: parsed_order[:products].first[:order_woo_id]
       )
-    end
 
-    it "updates a product sale related to the parsed product" do
       allow_any_instance_of(described_class).to receive(:verify_webhook).and_return(true)
 
       post "/update-order", params: request_body, headers: {"CONTENT_TYPE" => "application/json"}
+    end
 
+    it "updates a product sale related to the parsed product" do
+      parsed_product = parsed_order[:products].first
       updated_product_sale = ProductSale.find(@product_sale.id)
 
-      expect(updated_product_sale.qty).to eq(1)
-      expect(updated_product_sale.price).to eq(BigDecimal("235"))
-      expect(updated_product_sale.variation.title).to eq("Deluxe Version")
+      expect(updated_product_sale.qty).to eq(parsed_product[:qty])
+      expect(updated_product_sale.price).to eq(
+        BigDecimal(parsed_product[:price])
+      )
+      expect(updated_product_sale.variation.title).to eq(
+        parsed_product[:variation][:display_value]
+      )
+    end
+
+    it "creates a new sale when it doesn't exist" do
+      parsed_product = parsed_order[:products].last
+      new_sale = ProductSale.last
+
+      expect(new_sale.qty).to eq(parsed_product[:qty])
+      expect(new_sale.price).to eq(
+        BigDecimal(parsed_product[:price])
+      )
+      expect(new_sale.variation.title).to eq(
+        parsed_product[:variation][:display_value]
+      )
     end
   end
   # rubocop:enable RSpec/ExampleLength
