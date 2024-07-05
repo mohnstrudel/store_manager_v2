9da67fbdeb322969a1dc28f23b9091b7187b1a02
Yevgeny Matveyev <yev_mtv@icloud.com>
2024-07-05 20:14:13 +0800

    Check the most likely option first

--- app/jobs/sync_woo_orders_job.rb
+++ app/jobs/sync_woo_orders_job.rb
@@ -136,7 +136,7 @@ class SyncWooOrdersJob < ApplicationJob
   end
 
   def get_customer_id(parsed_customer)
-    customer = if parsed_customer[:woo_id].in? ["0", 0, ""]
+    customer = if parsed_customer[:woo_id].in? [0, "0", ""]
       Customer.find_or_initialize_by(
         email: parsed_customer[:email]
       )
