8c48ced286cc1d8baaa3a95d9bc81f552235a265
Yevgeny Matveyev <yev_mtv@icloud.com>
2024-07-11 17:30:56 +0800

    Use shorter syntax

--- spec/system/general_spec.rb
+++ spec/system/general_spec.rb
@@ -82,50 +82,50 @@ RSpec.describe "Search, create, and destroy", js: "true" do
     it "shows two sales and two suppliers debts in index" do
       visit root_path
 
-      expect(page).to have_content(@batman.full_title)
-      expect(page).to have_content(@guts.full_title)
-      expect(page).to have_content(@supplier_fnc.title)
-      expect(page).to have_content(@supplier_agk.title)
+      expect(page).to have_text(@batman.full_title)
+      expect(page).to have_text(@guts.full_title)
+      expect(page).to have_text(@supplier_fnc.title)
+      expect(page).to have_text(@supplier_agk.title)
     end
 
     it "finds one product in Debts page" do
       visit debts_path
 
-      expect(page).to have_content(@batman.full_title)
-      expect(page).to have_content(@guts.full_title)
+      expect(page).to have_text(@batman.full_title)
+      expect(page).to have_text(@guts.full_title)
 
       fill_in "q", with: @batman.title
       find_by_id("q").native.send_keys(:return)
 
-      expect(page).to have_content(@batman.full_title)
-      expect(page).to have_no_content(@guts.full_title)
+      expect(page).to have_text(@batman.full_title)
+      expect(page).to have_no_text(@guts.full_title)
     end
 
     it "finds one customer in Sales page" do
       visit sales_path
 
-      expect(page).to have_content(@laura_palmer.email)
-      expect(page).to have_content(@dale_cooper.email)
+      expect(page).to have_text(@laura_palmer.email)
+      expect(page).to have_text(@dale_cooper.email)
 
       fill_in "q", with: @laura_palmer.email
       find_by_id("q").native.send_keys(:return)
 
-      expect(page).to have_content(@laura_palmer.email)
-      expect(page).to have_no_content(@dale_cooper.email)
+      expect(page).to have_text(@laura_palmer.email)
+      expect(page).to have_no_text(@dale_cooper.email)
     end
 
     it "finds one purchase in Purchases page" do
       visit purchases_path
 
-      expect(page).to have_content(@asuka.full_title)
-      expect(page).to have_content(@batman.full_title)
+      expect(page).to have_text(@asuka.full_title)
+      expect(page).to have_text(@batman.full_title)
 
       fill_in "q", with: @asuka.full_title
       find_by_id("q").native.send_keys(:return)
 
-      expect(page).to have_content(@asuka.full_title)
-      expect(page).to have_no_content(@batman.full_title)
-      expect(page).to have_no_content(@guts.full_title)
+      expect(page).to have_text(@asuka.full_title)
+      expect(page).to have_no_text(@batman.full_title)
+      expect(page).to have_no_text(@guts.full_title)
     end
 
     it "doesn't break debts when we change products" do
@@ -144,8 +144,8 @@ RSpec.describe "Search, create, and destroy", js: "true" do
       fill_in "q", with: @batman.title
       find_by_id("q").native.send_keys(:return)
 
-      expect(page).to have_content(@batman.full_title)
-      expect(page).to have_content(6)
+      expect(page).to have_text(@batman.full_title)
+      expect(page).to have_text(6)
 
       find("tr[data-table-id-param='#{@batman.id}']").click
       find(:link, "Edit").click
@@ -160,8 +160,8 @@ RSpec.describe "Search, create, and destroy", js: "true" do
       fill_in "q", with: dc_comics.title
       find_by_id("q").native.send_keys(:return)
 
-      expect(page).to have_content(Product.find(@batman.id).full_title)
-      expect(page).to have_content(6)
+      expect(page).to have_text(Product.find(@batman.id).full_title)
+      expect(page).to have_text(6)
     end
 
     it "doesn't break purchases when we change them" do
@@ -191,7 +191,7 @@ RSpec.describe "Search, create, and destroy", js: "true" do
       fill_in "q", with: @purchase_batman.order_reference
       find_by_id("q").native.send_keys(:return)
 
-      expect(page).to have_content(asuka_variation.title)
+      expect(page).to have_text(asuka_variation.title)
     end
   end
 
@@ -243,9 +243,9 @@ RSpec.describe "Search, create, and destroy", js: "true" do
       malenia_regular_tr = "tr[data-table-id-param='#{@malenia_regular.id}']"
       malenia_revealing_tr = "tr[data-table-id-param='#{@malenia_revealing.id}']"
 
-      expect(page).to have_content(@malenia.full_title)
-      expect(page).to have_content(@malenia_regular.title)
-      expect(page).to have_content(@malenia_revealing.title)
+      expect(page).to have_text(@malenia.full_title)
+      expect(page).to have_text(@malenia_regular.title)
+      expect(page).to have_text(@malenia_revealing.title)
 
       within malenia_tr do
         expect(find("td:nth-child(4)")).to have_text("0")
