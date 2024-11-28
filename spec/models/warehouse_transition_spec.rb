a1492e3cbcee5cf5e58a4f68b03ca3ef1d85fc31
Yevgeny Matveyev <yev_mtv@icloud.com>
2024-11-28 15:16:39 +0800

    Add a spec for WarehouseTransition

---
+++ spec/models/warehouse_transition_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe WarehouseTransition do
+  describe "associations" do
+    it "belongs to notification" do
+      association = described_class.reflect_on_association(:notification)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it "belongs to from_warehouse" do
+      association = described_class.reflect_on_association(:from_warehouse)
+      expect(association.macro).to eq(:belongs_to)
+      expect(association.options[:class_name]).to eq("Warehouse")
+    end
+
+    it "belongs to to_warehouse" do
+      association = described_class.reflect_on_association(:to_warehouse)
+      expect(association.macro).to eq(:belongs_to)
+      expect(association.options[:class_name]).to eq("Warehouse")
+    end
+  end
+
+  describe "validations" do
+    it "requires notification" do
+      transition = build(:warehouse_transition, notification: nil)
+      expect(transition).not_to be_valid
+    end
+
+    it "requires from and to warehouses" do
+      transition = build(:warehouse_transition, from_warehouse: nil, to_warehouse: nil)
+      expect(transition).not_to be_valid
+    end
+
+    it "validates uniqueness of transition for notification" do
+      existing = create(:warehouse_transition)
+      duplicate = build(:warehouse_transition,
+        notification: existing.notification,
+        from_warehouse: existing.from_warehouse,
+        to_warehouse: existing.to_warehouse)
+      expect(duplicate).not_to be_valid
+    end
+  end
+end
