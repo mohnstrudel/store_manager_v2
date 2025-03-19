cc5635e3158bce0a4a018bfc30c3faa9d511baeb
Yevgeny Matveyev <yev_mtv@icloud.com>
2025-03-19 20:28:11 +0800

    Parse more size types which we have on new shop

--- app/models/size.rb
+++ app/models/size.rb
@@ -14,13 +14,18 @@ class Size < ApplicationRecord
   has_many :products, through: :product_sizes
 
   def self.parse_size(product_title)
-    num_size = product_title.match(numeric_size_match)
-    num_size[0].tr("/", ":") if num_size.present?
+    num_sizes = product_title.scan(numeric_size_match).flatten
+
+    return if num_sizes.blank?
+
+    sizes = num_sizes.map { |size| size.tr("/", ":") }
+    (sizes.length > 1) ? sizes : sizes.first
   end
 
   def self.sanitize_size(product_title)
     num_size = product_title.match(numeric_size_match)
     converted_title = num_size[0].tr("/", ":") if num_size.present?
+
     if converted_title
       product_title.sub(num_size[0], converted_title)
     else
@@ -29,6 +34,8 @@ class Size < ApplicationRecord
   end
 
   def self.numeric_size_match
-    /1[\/:]([3456]|3\.5|1|7)/
+    # 1:2, 1:3, 1:4, 1:5, 1:6, 1:3.5, 1:1, 1:7, 1:10
+    # and 1/2, 1/3, etc.
+    /(1[\/:](?:[23456789]|3\.5|1[0]?))/
   end
 end
