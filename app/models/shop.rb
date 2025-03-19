b53dec2056c9b90870e2091829b77cd84cae9ff7
Yevgeny Matveyev <yev_mtv@icloud.com>
2025-03-19 20:27:15 +0800

    Part of the initital Shopify setup

---
+++ app/models/shop.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: shops
+#
+#  id             :bigint           not null, primary key
+#  access_scopes  :string
+#  shopify_domain :string           not null
+#  shopify_token  :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+class Shop < ActiveRecord::Base
+  include ShopifyApp::ShopSessionStorageWithScopes
+
+  def api_version
+    ShopifyApp.configuration.api_version
+  end
+end
