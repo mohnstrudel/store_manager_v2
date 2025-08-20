c355ec1518d96158bd531dfcdcf368d2e56e9176
Yevgeny Matveyev <yev_mtv@icloud.com>
2025-08-20 13:57:45 +0300

    Feat: Load Slim Select even after Turbo page reload

--- app/javascript/controllers/product_editions_controller.js
+++ app/javascript/controllers/product_editions_controller.js
@@ -8,38 +8,51 @@ export default class extends Controller {
   versions = [];
   colors = [];
   editions = [];
+  turboRenderHandler = () => this.initializeSlimSelects();
 
   connect() {
-    this.sizesSelect = new SlimSelect({
-      select: this.sizesTarget,
-      events: {
-        afterChange: this.setupAndRender("sizes"),
-      },
-    });
-    this.initializeSelected(this.sizesSelect, this.setupAndRender("sizes"));
-    this.versionsSelect = new SlimSelect({
-      select: this.versionsTarget,
-      events: {
-        afterChange: this.setupAndRender("versions"),
-      },
-    });
-    this.initializeSelected(
-      this.versionsSelect,
-      this.setupAndRender("versions"),
-    );
-    this.colorsSelect = new SlimSelect({
-      select: this.colorsTarget,
-      events: {
-        afterChange: this.setupAndRender("colors"),
-      },
-    });
-    this.initializeSelected(this.colorsSelect, this.setupAndRender("colors"));
+    this.initializeSlimSelects();
+    document.addEventListener("turbo:render", this.turboRenderHandler);
   }
 
   disconnect() {
-    this.sizesSelect.destroy();
-    this.versionsSelect.destroy();
-    this.colorsSelect.destroy();
+    document.removeEventListener("turbo:render", this.turboRenderHandler);
+    if (this.sizesSelect) this.sizesSelect.destroy();
+    if (this.versionsSelect) this.versionsSelect.destroy();
+    if (this.colorsSelect) this.colorsSelect.destroy();
+  }
+
+  initializeSlimSelects() {
+    if (this.hasSizesTarget) {
+      this.sizesSelect = new SlimSelect({
+        select: this.sizesTarget,
+        events: {
+          afterChange: this.setupAndRender("sizes"),
+        },
+      });
+      this.initializeSelected(this.sizesSelect, this.setupAndRender("sizes"));
+    }
+    if (this.hasVersionsTarget) {
+      this.versionsSelect = new SlimSelect({
+        select: this.versionsTarget,
+        events: {
+          afterChange: this.setupAndRender("versions"),
+        },
+      });
+      this.initializeSelected(
+        this.versionsSelect,
+        this.setupAndRender("versions"),
+      );
+    }
+    if (this.hasColorsTarget) {
+      this.colorsSelect = new SlimSelect({
+        select: this.colorsTarget,
+        events: {
+          afterChange: this.setupAndRender("colors"),
+        },
+      });
+      this.initializeSelected(this.colorsSelect, this.setupAndRender("colors"));
+    }
   }
 
   setupAndRender(target) {
@@ -52,6 +65,7 @@ export default class extends Controller {
 
   initializeSelected(slimSelector, renderer) {
     const selectedItems = slimSelector.getSelected();
+
     if (selectedItems.length > 0) {
       const options = slimSelector.getData();
       const selectedOptions = options.filter((option) => {
