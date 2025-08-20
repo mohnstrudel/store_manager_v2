6abc2b9cf928768742146d49b7c4c8e2983d43cb
Yevgeny Matveyev <yev_mtv@icloud.com>
2025-08-20 15:06:31 +0300

    Feat: Add Turbo reload support to other slim-selects

--- app/javascript/controllers/slim_select_controller.js
+++ app/javascript/controllers/slim_select_controller.js
@@ -3,9 +3,20 @@ import SlimSelect from "slim-select";
 
 // Connects to data-controller="slim-select"
 export default class extends Controller {
+  turboRenderHandler = () => {
+    this.init();
+  };
   connect() {
-    new SlimSelect({
+    this.init();
+    document.addEventListener("turbo:render", this.turboRenderHandler);
+  }
+  init() {
+    this.slimSelect = new SlimSelect({
       select: this.element,
     });
+  }
+  disconnect() {
+    document.removeEventListener("turbo:render", this.turboRenderHandler);
+    if (this.slimSelect) this.slimSelect.destroy();
   }
 }
