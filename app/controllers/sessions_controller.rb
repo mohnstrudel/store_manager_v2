e35cdb8716540ff0f19f0fc0a3a06b5ba297e1d6
Yevgeny Matveyev <yev_mtv@icloud.com>
2025-09-03 10:27:00 +0300

    fix: Extend authorize callbacks to include destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   before_action :redirect_if_authenticated, only: :new
   allow_unauthenticated_access only: %i[new create]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later" }
-  skip_before_action :authorize_resourse, only: %i[new create]
-  skip_after_action :verify_authorized, only: %i[new create]
+  skip_before_action :authorize_resourse, only: %i[new create destroy]
+  skip_after_action :verify_authorized, only: %i[new create destroy]
 
   def new
   end
