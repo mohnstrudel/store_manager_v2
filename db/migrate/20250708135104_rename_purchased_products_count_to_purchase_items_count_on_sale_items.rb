7c320b5349b7b39f912d74f68a9506fd71821594
Yevgeny Matveyev <yev_mtv@icloud.com>
2025-09-03 19:10:04 +0300

    fix: Remove data migration for purchased_products_count in sale_items migration

--- db/migrate/20250708135104_rename_purchased_products_count_to_purchase_items_count_on_sale_items.rb
+++ db/migrate/20250708135104_rename_purchased_products_count_to_purchase_items_count_on_sale_items.rb
@@ -1,10 +1,6 @@
 class RenamePurchasedProductsCountToPurchaseItemsCountOnSaleItems < ActiveRecord::Migration[8.0]
   def up
     add_column :sale_items, :purchase_items_count, :integer, default: 0, null: false
-    SaleItem.reset_column_information
-    SaleItem.find_each do |item|
-      item.update_column(:purchase_items_count, item.purchased_products_count)
-    end
   end
 
   def down
--- db/migrate/20250709050626_remove_purchased_products_count_from_sale_items.rb
+++ db/migrate/20250709050626_remove_purchased_products_count_from_sale_items.rb
@@ -5,9 +5,5 @@ class RemovePurchasedProductsCountFromSaleItems < ActiveRecord::Migration[8.0]
 
   def down
     add_column :sale_items, :purchased_products_count, :integer, default: 0, null: false
-    SaleItem.reset_column_information
-    SaleItem.find_each do |item|
-      item.update_column(:purchased_products_count, item.purchase_items_count)
-    end
   end
 end
