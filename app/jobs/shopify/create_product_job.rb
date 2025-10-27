7ee136b18773d873f0c411e24f33500da439c5a5
Yevgeny Matveyev <yev_mtv@icloud.com>
2025-10-27 20:40:50 +0300

    Simplify Shopify product creation flow
    - Remove intermediate status updates during product creation - Store
    product slug instead of full page URL

--- app/jobs/shopify/create_product_job.rb
+++ app/jobs/shopify/create_product_job.rb
@@ -5,21 +5,14 @@ class Shopify::CreateProductJob < ApplicationJob
 
     if serialized_product.present?
       api_client = Shopify::ApiClient.new
-
       store_info = product.store_infos.find_or_initialize_by(name: :shopify)
-      store_info.assign_attributes(
-        push_status: :pending,
-        push_time: Time.current
-      )
-      store_info.save!
 
       product_response = api_client.create_product(serialized_product)
 
       store_info.assign_attributes(
-        push_status: :done,
         push_time: Time.current,
         store_product_id: product_response["id"],
-        page_url: "https://handsomecake.com/products/#{product_response["handle"]}"
+        slug: product_response["handle"]
       )
       store_info.save!
     end
