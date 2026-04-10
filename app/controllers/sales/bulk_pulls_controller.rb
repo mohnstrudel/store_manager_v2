69fa7cb9d73a794be079f987291a56623d35443a
Yevgeny Matveyev <yev_mtv@icloud.com>
2026-04-10 11:45:44 +0300

    fix(sales): use store info ids for pull jobs

--- app/controllers/sales/bulk_pulls_controller.rb
+++ app/controllers/sales/bulk_pulls_controller.rb
@@ -8,10 +8,11 @@ module Sales
       limit = params[:limit]
 
       Config.update_shopify_sales_sync_time
+
       Shopify::PullSalesJob.perform_later(limit:)
       Woo::PullSalesJob.set(wait: 90.seconds).perform_later(limit:)
-      set_jobs_status_notice!
 
+      set_jobs_status_notice!
       redirect_back_or_to(sales_path)
     end
 
