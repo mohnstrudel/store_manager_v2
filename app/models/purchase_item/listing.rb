78d65c859b9a05012ec31c64886c0e5b40508b98
Yevgeny Matveyev <yev_mtv@icloud.com>
2026-06-04 00:50:49 +0300

    perf(purchase-item): preload associations for detail views

--- app/models/purchase_item/listing.rb
+++ app/models/purchase_item/listing.rb
@@ -9,17 +9,23 @@ module PurchaseItem::Listing
     scope :with_media, -> { includes(media: {image_attachment: :blob}) }
 
     scope :for_purchase_details, -> {
-      includes(:warehouse, :sale_item, purchase: :payments, sale: [:customer, :shopify_info, :woo_info])
+      includes(
+        :warehouse,
+        :shipping_company,
+        :sale_item,
+        purchase: :payments,
+        sale: [:customer, :shopify_info, :woo_info, :shipping_address]
+      )
     }
 
     scope :for_warehouse_details, -> {
       includes(
         :shipping_company,
-        sale: :customer,
+        sale: [:customer, :shipping_address],
         purchase: [
           :payments,
           :purchase_items,
-          :variant,
+          {variant: [:size, :version, :color]},
           {product: :variants}
         ]
       )
